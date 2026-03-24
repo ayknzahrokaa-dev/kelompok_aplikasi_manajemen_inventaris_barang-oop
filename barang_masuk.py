a0dc616ce888771a55bbe634412af65c6e80ba6a
ayknzahrokaa-dev <ayikkuniizahrokaa@gmail.com>
2026-03-23 22:34:47 -0700

    barang_masuk

---
+++ barang_masuk.py
@@ -0,0 +1,26 @@
+from abstract_transaksi import AbstractTransaksi
+
+class BarangMasuk(AbstractTransaksi):
+
+    def __init__(self, id_transaksi, barang, jumlah):
+        super().__init__(id_transaksi)
+        self.__barang = barang
+        self.set_jumlah(jumlah)
+
+    def get_barang(self):
+        return self.__barang
+
+    def get_jumlah(self):
+        return self.__jumlah
+
+    def set_jumlah(self, jumlah):
+        if jumlah <= 0:
+            raise ValueError("Jumlah harus positif")
+        self.__jumlah = jumlah
+
+    def proses(self):
+        try:
+            self.__barang.tambah_stok(self.__jumlah)
+            print("Barang masuk berhasil")
+        except Exception as e:
+            print("Error:", e)
